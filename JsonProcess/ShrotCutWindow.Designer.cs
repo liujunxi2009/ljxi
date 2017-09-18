47f59a9387f466b7f38973a5da65af8d1cc3fa59
liujunxi2009 <liujunxi2009@163.com>
2017-09-18 16:06:27 +0800

    test cmd

--- JsonProcess/ShrotCutWindow.Designer.cs
+++ JsonProcess/ShrotCutWindow.Designer.cs
@@ -71,7 +71,7 @@
       this.button3.Name = "button3";
       this.button3.Size = new System.Drawing.Size(80, 53);
       this.button3.TabIndex = 3;
-      this.button3.Text = "命令行";
+      this.button3.Text = "远程连接";
       this.button3.UseVisualStyleBackColor = true;
       this.button3.Click += new System.EventHandler(this.button3_Click);
       // 
--- JsonProcess/ShrotCutWindow.cs
+++ JsonProcess/ShrotCutWindow.cs
@@ -50,8 +50,25 @@ namespace JsonProcess
 
     private void button4_Click(object sender, EventArgs e)
     {
-      string start_elac = @"C:\Windows\system32\SnippingTool.exe";
-      usedoscmd(start_elac);
+      Process p = new Process();  // 初始化新的进程
+      p.StartInfo.FileName = "CMD.EXE"; //创建CMD.EXE 进程
+      p.StartInfo.RedirectStandardInput = true; //重定向输入
+      p.StartInfo.RedirectStandardOutput = true;//重定向输出
+      p.StartInfo.UseShellExecute = false; // 不调用系统的Shell
+      p.StartInfo.RedirectStandardError = true; // 重定向Error
+      p.StartInfo.CreateNoWindow = true; //不创建窗口
+      p.Start(); // 启动进程
+      p.StandardInput.WriteLine("call C:\\Windows\\system32\\mspaint.exe"); // Cmd 命令
+      p.StandardInput.WriteLine("exit"); // 退出
+      string s = p.StandardOutput.ReadToEnd(); //将输出赋值给 S
+      p.WaitForExit();  // 等待退出
+
+
+
+
+
+      //string start_elac = @"C:\Windows\system32\SnippingTool.exe";
+      //usedoscmd(start_elac);
     }
   }
 }
